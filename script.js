69d85f8f14c76cca8e576d7daa9239e61e440936
Anil Kumar <anilbms75@gmail.com>
2021-03-09 14:34:33 +0100

    Populate numbers using JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = 'Naveen';
+document.querySelector('#name').innerText = name;
+
+// Add number 1 to 10
+for (var i = 1; i < 11; i++) {
+    document.querySelector('.numbers').innerHTML += "<div>" + i + "</div>";
+}
